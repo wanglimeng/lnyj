638e907f1ffaf6384949682e338b57d96ec5ce00
wanglimeng <1592138069@qq.com>
2017-03-08 11:08:13 +0800

    test

--- app.js
+++ app.js
@@ -7,50 +7,41 @@ const serve = require('koa-static');
 const favicon = require('koa-favicon');
 const logger = require('koa-logger');
 const session = require('koa-generic-session');
-const MysqlStore = require('koa-mysql-session')
-const path = require('path');
-const users = require('./models/usersModels.js');
-const config = require('./config/config.js');
+const MysqlStore = require('koa-mysql-session');
+const onerror = require('koa-onerror');
 
+
+//错误处理
+onerror(app);
 app.keys = ['SESSIONID'];
+
+//mysqlSession中间件
 app.use(session({
 	store: new MysqlStore(config.mysqlSession),
 	rolling: true,
 	cookie: {
 		maxage: 30* 60 * 1000
 	}
-}))
+}));
+
+//日志中间件
 app.use(logger());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(views(path.join(__dirname, './views'), {
   extension: 'ejs'
 }));
+
+
 app.use(koaBody({
 	formidable:{uploadDir: './public'},
 	multipart: true
 }));
+
+//静态文件服务
 app.use(serve(__dirname + '/public'));
 
 
 
-router.get('/users',async function (ctx,next) {
-	let res = await users.findOne();
-	await ctx.render('users',{
-		username: res.dataValues.username,
-		password: res.dataValues.password
-
-	});
-});
-
-router.post('/users' ,async function (ctx,next) {
-	console.log(ctx.request.body);
-	ctx.body = 'wanglimeng';
-});
-router.get('/session', async function (ctx) {
-	ctx.session.test = 'test';
-	ctx.body = ctx.session.test;
-});
-app.use(router.routes());
 
 
 
